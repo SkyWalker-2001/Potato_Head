da4fafd9d691a417310edd0f983f8e3fe3ef3c7f
SkyWalker-2001 <126860277+SkyWalker-2001@users.noreply.github.com>
2024-02-13 22:11:30 -0800

    Gun Animation COMP

--- Assets/Scripts/Gun/Gun.cs
+++ Assets/Scripts/Gun/Gun.cs
@@ -1,15 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
+    public static Action OnShoot;
+
     public Transform BulletSpawnPoint => _bulletSpawnPoint;
 
     [SerializeField] private Transform _bulletSpawnPoint;
     [SerializeField] private Bullet _bulletPrefab;
+    [SerializeField] private float _gunFireCD = .5f;
 
+    private static readonly int FIRE_HASH = Animator.StringToHash("Fire");
     private Vector2 _mousPos;
+    private float _lastFireTime = 0f;
+
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
 
     private void Update()
     {
@@ -17,17 +30,43 @@ public class Gun : MonoBehaviour
         RotateGun();
     }
 
+    private void OnEnable()
+    {
+        OnShoot += FireAnimation;
+        OnShoot += ShootProjectile;
+        OnShoot += ResetShootCD;
+    }
+
+    private void OnDisable()
+    {
+        OnShoot -= FireAnimation;
+        OnShoot -= ShootProjectile;
+        OnShoot -= ResetShootCD;
+    }
+
     private void Shoot()
     {
-        if (Input.GetMouseButtonDown(0)) {
-            ShootProjectile();
+        if (Input.GetMouseButton(0) && Time.time >= _lastFireTime)
+        {
+            OnShoot.Invoke();
         }
     }
+
+    private void ResetShootCD()
+    {
+        _lastFireTime = Time.time + _gunFireCD;
+    }
+
 
     private void ShootProjectile()
     {
         Bullet newBullet = Instantiate(_bulletPrefab, _bulletSpawnPoint.position, Quaternion.identity);
         newBullet.Init(_bulletSpawnPoint.position, _mousPos);
+    }
+
+    private void FireAnimation()
+    {
+        _animator.Play(FIRE_HASH, 0, 0f);
     }
 
     private void RotateGun()
